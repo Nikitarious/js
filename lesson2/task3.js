4abfd575644975d809cf766d73dedffac6fe5610
Nikitarious <59883385+Nikitarious@users.noreply.github.com>
2021-05-31 16:27:18 +0300

    Add files via upload

---
+++ lesson2/task3.js
@@ -0,0 +1,11 @@
+let a = Number(prompt("Введите а"));
+let b = Number(prompt("Введите b"));
+
+if (a >= 0 && b  >= 0)
+    alert (a - b);
+
+if (a < 0 && b  < 0)
+    alert (a * b);
+
+if (a * b  < 0)
+    alert (a + b);
