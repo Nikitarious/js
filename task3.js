8359b788b5c2df38482f0d11ac6bd5fdc5a312b6
Nikitarious <59883385+Nikitarious@users.noreply.github.com>
2021-06-03 17:44:55 +0300

    Update task3.js

--- task3.js
+++ task3.js
@@ -1,3 +1,18 @@
-let name = "Василий";
-let admin = name;
-console.log(admin);
+const products = [
+    {
+    id: 3,
+    price: 200,
+    },
+    {
+    id: 4,
+    price: 900,
+    },
+    {
+    id: 1,
+    price: 1000,
+    },
+    ];
+
+for (let i = 0; i < 3; i++) {
+    console.log(products[i].price * 0.85);
+}
