f168040f2cbe4d8cf5c9739197a04ddaae9630c3
Nikitarious <59883385+Nikitarious@users.noreply.github.com>
2021-06-03 17:44:26 +0300

    Update task1.js

--- task1.js
+++ task1.js
@@ -1,3 +1,12 @@
-let name = "Василий";
-let admin = name;
-console.log(admin);
+for (let i = 0; i < 11; i++) {
+    if (i == 0){
+        console.log("0 - это ноль");   
+  
+    }
+    if (i % 2 == 0 &&  i != 0){
+        console.log(i +" - это четное" );
+    }
+    if (i % 2 == 1 &&  i != 0){
+        console.log(i +" - это нечетное" );
+    }
+}
